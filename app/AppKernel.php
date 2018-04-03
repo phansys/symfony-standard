f6b3c3b32935f096e48f9bead6a2c6d6f2d920e6
Javier Spagnoletti <phansys@gmail.com>
2018-04-03 16:41:43 -0300

    Circular reference detected for service "routing.loader" when `AsseticBundle` is enabled at `AppKernel::registerBundles()`

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,6 +9,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
